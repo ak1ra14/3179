60e85eab6789c0812ec7ad6e5574d1a41706d839
Akira Sato <126305563+ak1ra14@users.noreply.github.com>
2024-10-09 22:37:49 +0800

    For week11 critique

--- vegalite_viz.js
+++ vegalite_viz.js
@@ -6,4 +6,16 @@ var vg_2 = "bubble_chart.vg.json";
 vegaEmbed("#bubble", vg_2).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+var vg_3 = "heatmap.vg.json";
+vegaEmbed("#heatmap", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_4 = "donut_chart.vg.json";
+vegaEmbed("#donut", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_5 = "donut_chart_my.vg.json";
+vegaEmbed("#donut_my", vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
 
