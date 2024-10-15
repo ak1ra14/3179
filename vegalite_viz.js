57c642d0d1e741c62146fa877fdf5c29bd07c672
Akira Sato <126305563+ak1ra14@users.noreply.github.com>
2024-10-15 11:56:34 +0800

    Finalisation for data vis 2

--- vegalite_viz.js
+++ vegalite_viz.js
@@ -10,12 +10,38 @@ var vg_3 = "heatmap.vg.json";
 vegaEmbed("#heatmap", vg_3).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
-var vg_4 = "donut_chart.vg.json";
-vegaEmbed("#donut", vg_4).then(function(result) {
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
-}).catch(console.error);
 var vg_5 = "donut_chart_my.vg.json";
 vegaEmbed("#donut_my", vg_5).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+var vg_7 = "donut_chart.vg.json";
+vegaEmbed("#donut", vg_7).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_8 = "stacked_areachart.vg.json";
+vegaEmbed("#area_chart", vg_8).then(function(result) {
+   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var vg_6 = "combined_charts.vg.json";
+vegaEmbed("#combined_charts", vg_6).then(function(result) {
+   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+// Load the JSON file
 
+
+// // Load the combined specification
+// vegaEmbed("#combined_chart", vg_6).then(function(result) {
+//     // Extract the vconcat array from the spec
+//     const spec = result.spec;
+
+//     // Embed the first chart (area chart)
+//     vegaEmbed("#area_chart", spec.vconcat[0]).then(function(result) {
+//         console.log("Area chart loaded");
+//     }).catch(console.error);
+
+//     // Embed the second chart (donut chart)
+//     vegaEmbed("#donut", spec.vconcat[1]).then(function(result) {
+//         console.log("Donut chart loaded");
+//     }).catch(console.error);
+
+// }).catch(console.error);
